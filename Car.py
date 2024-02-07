aa564dacf5871e51a9bb06f47a46215fdf54a9f1
Aditya Sriram <157142601+Turtl3God7@users.noreply.github.com>
2024-02-07 10:29:08 -0600

    Added support files for new car database.
    Thanks @Magnus-Thomsen !

---
+++ Car.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, brand, price, year, licensePlate, isLeasingCar):
+        self.brand = brand
+        self.price = price
+        self.year = year
+        self.licensePlate = licensePlate
+        self.isLeasingCar = isLeasingCar
